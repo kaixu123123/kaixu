aa6a7f62bff58aaeccd1601dd521eaf243df3c2f
xk <1985341484@qq.com>
2024-07-31 16:47:24 +0800

    0729

---
+++ bin/0729/4.c
@@ -0,0 +1,35 @@
+#include    <stdio.h>
+void sort(int arr[] , int sz)
+{
+    int i = 0;
+    for (i = 0; i < sz-1; i++)
+    {
+        int j = 0;
+        for (j = 0; j < sz-1-i; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+
+            }
+            
+        }
+        
+    }
+    
+}
+int main()
+{   
+    int arr[] = {9,8,7,6,5,4,3,2,1,0};
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    sort(arr,sz);
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+         printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
